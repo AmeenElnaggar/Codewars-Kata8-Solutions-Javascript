661ceec93182e3cfa7bd914f7020e4458ff353a2
Ameen Sayed <ameenelnaggar9@gmail.com>
2024-07-28 12:39:44 +0300

    Add New File

---
+++ Return the day.js
@@ -0,0 +1,35 @@
+// Code wars Kata8 Solutions
+
+/*
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+*/
+
+/*
+    1) Create object with the days and nums
+    2) Check if the object include the num or no
+*/
+
+//Solution
+function whatday(num) {
+  const week = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday',
+  };
+
+  return week[num] ?? 'Wrong, please enter a number between 1 and 7';
+}
+console.log(whatday(10));
