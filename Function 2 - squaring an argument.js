cc34374445dbe99a65887e571c7cadcdffe6c3e9
Ameen Sayed <ameenelnaggar9@gmail.com>
2024-10-29 18:28:43 +0300

    Add New File

---
+++ Function 2 - squaring an argument.js
@@ -0,0 +1,11 @@
+// Code wars Kata8 Solutions
+
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+//Solution
+function square(n) {
+  return n ** 2;
+}
+console.log(square(5));
